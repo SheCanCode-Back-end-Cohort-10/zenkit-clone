40eea02dca44604dda3092a85169a073aec8fdb5
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-22 00:23:07 +0200

    Refactoring: Updating the setTime function

--- src/middlewares/time.js
+++ src/middlewares/time.js
@@ -3,22 +3,32 @@ import { durationCalculator } from '../utils/helperFunctions.js';
 export const setTime = async (req, res, next) => {
     var startTime = "";
     var endTime = "";
+
     if (!req.body.dueDate) {
+        req.body.dueDate = { 
+            startDate: new Date()
+        };
+        console.log("Start date: ",req.body.dueDate.startDate.toString());
+        startTime = new Date(req.body.dueDate.startDate.toString().slice(0, -1)).toLocaleTimeString();
+        req.body.dueDate.startTime = startTime;
         next();
     } else {
         if (req.body.dueDate.startDate) {
             startTime = new Date(req.body.dueDate.startDate.slice(0, -1)).toLocaleTimeString();
+            req.body.dueDate.startTime = startTime;
         }
+
         if (req.body.dueDate.endDate) {
             endTime = new Date(req.body.dueDate.endDate.slice(0, -1)).toLocaleTimeString();
+            req.body.dueDate.endTime = endTime;
         }
-        req.body.dueDate.startTime = startTime;
-        req.body.dueDate.endTime = endTime;
-    
-        const duration = durationCalculator(req.body.dueDate.startDate, req.body.dueDate.endDate);
-        req.body.dueDate.duration = duration.durationPeriod;
-        req.body.dueDate.durationType = duration.durationType;
-    
+
+        if (req.body.dueDate.startDate && req.body.dueDate.endDate && !req.body.dueDate.duration) {
+            const duration = durationCalculator(req.body.dueDate.startDate, req.body.dueDate.endDate);
+            req.body.dueDate.duration = duration.durationPeriod;
+            req.body.dueDate.durationType = duration.durationType;
+        }
+
         next();
     }
 }
