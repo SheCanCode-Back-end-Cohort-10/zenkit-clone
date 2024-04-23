89ac7f8bc8205a014cf42da0c4d7ccd59849e78b
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-23 15:02:32 +0200

    Feature: OPT Verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const userRouter = express.Router();
-import { SignUp, SignIn } from '../controllers/user.controller.js';
-import { signUpValidations, signInValidations } from '../utils/validation.js';
+import { SignUp, SignIn, ValidateOpt } from '../controllers/user.controller.js';
+import { signUpValidations, signInValidations, otpValidation } from '../utils/validation.js';
 
 userRouter.post('/signup', signUpValidations, SignUp);
 userRouter.post('/signin', signInValidations, SignIn);
+userRouter.post('/verify', otpValidation, ValidateOpt);
 
 export default userRouter;
---
+++ src/utils/otp.js
@@ -0,0 +1,7 @@
+export const otpGenerator = () => {
+    var otp = 0;
+
+    otp = Math.ceil(Math.random()*1000000);
+
+    return otp;
+};
